2ad101bb9899b8b9425ba669a8a0f3e16c5632aa
CMVGiner <christabelggg@gmail.com>
2026-07-22 20:41:47 -0700

    added code to reservable.java

--- src/librarymanagementsystem/reservable.java
+++ src/librarymanagementsystem/reservable.java
@@ -2,4 +2,7 @@ package librarymanagementsystem;
 
 public interface reservable {
 
+boolean reserveItem(String itemId, int timeSlot, String userID);
+void checkReservationStatus(String itemId);
+  
 }
