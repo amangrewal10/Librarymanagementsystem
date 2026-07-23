0ca3d13905dc5c8b2e3d99249e53b5e88829d897
CMVGiner <christabelggg@gmail.com>
2026-07-22 20:12:02 -0700

    added code for book.java

--- src/librarymanagementsystem/book.java
+++ src/librarymanagementsystem/book.java
@@ -1,11 +1,37 @@
 package librarymanagementsystem;
+import java.time.LocalDate;
 
 public class book extends libraryitem implements borrowable {
     private Genre genre;
     private int borrowperiod;
     private String ISBN;
     private int numbercopies;
+    private ArrayList<LocalDate> borrowingdates = new ArrayList<LocalDate>();
+    private ArrayList<String> borroweduserIDs = new ArrayList<String>();
     
-    
+public double calculateBorrowingFee(int loanperiod, double rateperday){
+rateperday= 1.50;
+    return loanperiod*rateperday;
+}
 
+public LocalDate processBorrowing (ArrayList<LocalDate> borrowingdates){
+
+}
+
+public int calculateLateFee(){
+
+}
+
+public int validateBorrowedDays(){
+
+}
+    
+public String validateISBN(String ISBN){
+
+}
+
+public String getItemType(String Book){
+return Book;
+}
+    
 }
