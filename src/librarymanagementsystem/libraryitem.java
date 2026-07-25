ab8b9d812919ae0aaafcf9b9ba92608713709922
CMVGiner <christabelggg@gmail.com>
2026-07-25 04:27:35 -0700

    added code

--- src/librarymanagementsystem/libraryitem.java
+++ src/librarymanagementsystem/libraryitem.java
@@ -1,32 +1,54 @@
 package librarymanagementsystem;
+import java.time.LocalTime;
 
 abstract class libraryitem {
     private long id;
     private String title;
     private String author;
-    private int yearpublished;
+    private int yearPublished;
+
+    public libraryItem(long id,String title,String author,int yearPublished){
+        super();
+        setID(id);
+        setTitle(title);
+        setAuthor(author);
+        setYearPublished(yearPublished);
+    }
     
-    
-    public String validateTitle;
-    public  int validateYear;
-    public String getItemType;
-    
-    public long getId;
-    public long setID;
-    public String getTtile;
-    public String setTtile;
-    public String getAuthor;
-    public String setAuthor;
-    public int getyearpublished;
-    public int setyearpublished;
-    
-    
+    public String validateTitle(){
         
+    }
+    public  int validateYear(){
+
+    }
+    public abstract void getItemType(){
+
+    }
     
-    
-    
-    
-    
-    
+    public String getId(){
+return id;
+    }
+    public void setID(long id){
+this.id=id;
+    }
+    public String getTtile(){
+return title;
+    }
+    public void setTtile(String title){
+this.title=title;
+    }
+    public String getAuthor(){
+return author;
+    }
+    public void setAuthor(String author){
+this.author=author;
+    }
+    public String getYearpublished(){
+return yearPublished
+    }
+    public void setYearpublished(int yearPublished){
+this.yearPublished=yearPublished;
+    }
+        
 
 }
