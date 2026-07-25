5fd7e3b4f6c8df6522c990bdf4e54da62e02eb65
CMVGiner <christabelggg@gmail.com>
2026-07-25 03:03:27 -0700

    added code to magazine.java
    Implemented magazine class with reservable interface and reservation methods.

--- src/librarymanagementsystem/magazine.java
+++ src/librarymanagementsystem/magazine.java
@@ -1,5 +1,27 @@
 package librarymanagementsystem;
 
-public class magazine {
+public class magazine extends libraryitem implements reservable{
+private int issueNumber;
+private boolean monthlyEdition;
+private ArrayList<Integer> reservedSlots = new ArrayList<>();
+private ArrayList<String> reservedUserIDs = new ArrayList<>();  
+
+public boolean validateTimeSlot(int timeSlot){
+
+}
+
+public void reserveItem(){
+
+}
+
+public void checkReservationStatus(){
+
+}
+
+public String getItemType(String Magazine)
+  return Magazine;
+
+}
+
 
 }
