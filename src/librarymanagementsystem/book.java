00262ec8126c132c51ed2c039c9ae863b732d6da
CMVGiner <christabelggg@gmail.com>
2026-07-25 04:45:20 -0700

    added code
    Refactor book class attributes and methods for consistency.

--- src/librarymanagementsystem/book.java
+++ src/librarymanagementsystem/book.java
@@ -3,11 +3,17 @@ import java.time.LocalDate;
 
 public class book extends libraryitem implements borrowable {
     private Genre genre;
-    private int borrowperiod;
+    private int borrowPeriod;
     private String ISBN;
-    private int numbercopies;
-    private ArrayList<LocalDate> borrowingdates = new ArrayList<LocalDate>();
-    private ArrayList<String> borroweduserIDs = new ArrayList<String>();
+    private int numberCopies;
+    private int availableCopies
+    private ArrayList<LocalDate> borrowingDates = new ArrayList<LocalDate>();
+    private ArrayList<String> borrowedUserIDs = new ArrayList<String>();
+
+public book(String userID,int overdueDays,int loanPeriod){
+super(userID,overdueDays,loanPeriod);
+    
+}
     
 public double calculateBorrowingFee(int loanperiod, double rateperday){
 rateperday= 1.50;
@@ -33,5 +39,42 @@ public String validateISBN(String ISBN){
 public String getItemType(String Book){
 return Book;
 }
+
+public Genre getGenre (){
+return genre;
+}
+
+public void setGenre(Genre genre){
+this.genre=genre;
+}
+
+public int getBorrowedPeriod(){
+return borrowedPeriod;
+}
+
+public void setBorrowedPeriod(){
+this.borrowedPeriod=borrowedPeriod;
+}  
+
+public String getISBN(){
+return ISBN;
+}
+
+public void setISBN(){
+this.ISBN=ISBN;
+}   
+
+public int getNumberCopies(){
+return numberCopies;
+}   
+
+public void setNumberCopies(){
+this.numberCopies=numberCopies;
+}    
+
+public int availableCopies() {
+
+}
+    
     
 }
