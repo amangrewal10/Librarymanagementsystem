0ad19fb0f828884a682c6df99bba8178bbd3c66e
CMVGiner <christabelggg@gmail.com>
2026-07-25 22:25:29 -0700

    added code
    Added constructor and updated methods for reservations and issue number handling.

--- src/librarymanagementsystem/magazine.java
+++ src/librarymanagementsystem/magazine.java
@@ -6,15 +6,20 @@ private boolean monthlyEdition;
 private ArrayList<Integer> reservedSlots = new ArrayList<>();
 private ArrayList<String> reservedUserIDs = new ArrayList<>();  
 
+public magazine(long id, String title, String author, int yearPublished){
+super(id,title, author, yearPublished);
+
+}
+  
 public boolean validateTimeSlot(int timeSlot){
 
 }
 
-public void reserveItem(){
+public void reserveItem(String itemId, int timeSlot, String userID){
 
 }
 
-public void checkReservationStatus(){
+public void checkReservationStatus(String itemId){
 
 }
 
@@ -22,6 +27,23 @@ public String getItemType(String Magazine)
   return Magazine;
 
 }
+
+public int getIssueNumber(){
+return issueNumber;
+}
+
+public void setIssueNumber(int issueNumber){
+this.issueNumber=issueNumber;
+  }
+
+public boolean getMonthlyEdition(){
+  return monthlyEdition;
+}
+
+public void setMonthlyEdition(boolean monthlyEdition){
+this.monthlyEdition=monthlyEdition;
+  }
+
 
 
 }
