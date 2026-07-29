fff8859907a68c6411b285d80495db9ca3eb3b10
CMVGiner <christabelggg@gmail.com>
2026-07-28 22:01:51 -0700

    added code
    Added functionality to write admin account details to a file and read user input for admin credentials.

--- src/librarymanagementsystem/Testlibrary.java
+++ src/librarymanagementsystem/Testlibrary.java
@@ -2,8 +2,25 @@ package librarymanagementsystem;
 import java.util.*;
 import java.io.*;
 public class Testlibrary {
-ArrayList   
-BufferedWriter
+
+Scanner in = new Scanner(System.in);  
+ArrayList<String> readingItem = new ArrayList<>();
+
+File adminAccounts = new File(adminAccounts.txt);    
+
+  try{
+adminAccounts.write("adminLibrary,adminRead");
+adminAccounts.close();  
+}catch(IOException e ){
+System.out.println("Write error");
+e.printStackTrace();  
+}
+  
+System.out.println("Enter admin username);
+String adminUser = in.nextLine():
+
+System.out.println("Enter admin password);
+String adminPassword = in.nextLine():  
 
   
 }
